d845cb15ad0e6e4f3eafc69e0617840ba70dc28d
chenyouwei <youwei.chen@duobei.com>
2021-02-18 18:30:50 +0800

    feat(leetcode): 62 不同路径

--- src/main/java/com/leetcode/editor/cn/dp/Dp.java
+++ src/main/java/com/leetcode/editor/cn/dp/Dp.java
@@ -40,10 +40,29 @@ public class Dp {
         return dp[n];
     }
 
+    public int 机器人路径(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 0; i < n; i++){
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
     public static void main(String[] args) {
         Dp dp = new Dp();
-        int dp1 = dp.跳台阶(5);
-        int dp2 = dp.跳台阶2(5);
-        System.out.println(dp1 + "\n" + dp2);
+        // int dp1 = dp.跳台阶(5);
+        // int dp2 = dp.跳台阶2(5);
+        // System.out.println(dp1 + "\n" + dp2);
+
+        int dp2 = dp.机器人路径(7, 3);
+        System.out.println(dp2);
     }
 }
