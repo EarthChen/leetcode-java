f0a4cf87b1ec2956830d9c71a7e1e9376b0c1724
earthchen <earthchen1996@gmail.com>
2021-03-20 18:18:17 +0800

    feat(leetcode): 83 删除排序链表中的重复元素

---
+++ src/main/java/com/leetcode/editor/cn/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,64 @@
+package com.leetcode.editor.cn;
+
+//给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。 
+//
+// 示例 1: 
+//
+// 输入: 1->1->2
+//输出: 1->2
+// 
+//
+// 示例 2: 
+//
+// 输入: 1->1->2->3->3
+//输出: 1->2->3 
+// Related Topics 链表 
+// 👍 491 👎 0
+
+
+public class RemoveDuplicatesFromSortedList {
+    public static void main(String[] args) {
+        Solution solution = new RemoveDuplicatesFromSortedList().new Solution();
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode() {} ListNode(int
+     * val) { this.val = val; } ListNode(int val, ListNode next) { this.val = val; this.next = next; } }
+     */
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            ListNode current = head;
+            while (current != null && current.next != null) {
+                int val = current.val;
+                ListNode next = current.next;
+                if (next.val != val) {
+                    current = next;
+                } else {
+                    current.next = next.next;
+                }
+            }
+            return head;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
