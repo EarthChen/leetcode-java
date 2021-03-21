4781a7a1d84262a06db1b6f60f06a219f4a970e6
earthchen <earthchen1996@gmail.com>
2021-03-21 17:58:22 +0800

    feat(剑指offer): 18 删除链表的节点

---
+++ src/main/java/com/leetcode/editor/cn/list/ListNode.java
@@ -0,0 +1,23 @@
+package com.leetcode.editor.cn.list;
+
+/**
+ * @author earthchen
+ * @date 2021/3/21
+ **/
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public ListNode(int x) {
+        val = x;
+    }
+}
