b78bc2a157c7ce8f9ecc50db5b68e7e3af46d3e7
earthchen <earthchen1996@gmail.com>
2021-04-03 16:53:41 +0800

    feat(leetcode): 105 增加后序中序构造二叉树解法

--- src/main/java/com/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/main/java/com/leetcode/editor/cn/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -29,7 +29,13 @@ import java.util.Map;
 public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
     public static void main(String[] args) {
         Solution solution = new ConstructBinaryTreeFromPreorderAndInorderTraversal().new Solution();
-        solution.buildTree(new int[]{3, 9, 20, 15, 7}, new int[]{9, 3, 15, 20, 7});
+        int[] preorder = new int[]{3, 9, 20, 15, 7};
+        int[] inorder = new int[]{9, 3, 15, 20, 7};
+        int[] tailOrder = new int[]{9, 15, 7, 20, 3};
+        TreeNode root = solution.buildTree(preorder, inorder);
+        TreeNode root2 = solution.buildTailTree(tailOrder, inorder);
+        TreeNode root3 = solution.buildTailTree(tailOrder, inorder);
+        System.out.println();
     }
     //leetcode submit region begin(Prohibit modification and deletion)
 
@@ -57,6 +63,63 @@ public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
             int len = inRootIndex - inStart;
             root.left = build(preorder, valIndexMap, preStart + 1, preStart + len, inStart);
             root.right = build(preorder, valIndexMap, preStart + len + 1, preEnd, inRootIndex + 1);
+            return root;
+        }
+
+        public TreeNode buildTailTree(int[] tailOrder, int[] inorder) {
+            Map<Integer, Integer> valIndexMap = new HashMap<>();
+            for (int i = 0; i < inorder.length; i++) {
+                valIndexMap.put(inorder[i], i);
+            }
+            return buildTail(tailOrder, valIndexMap, 0, tailOrder.length - 1, 0);
+        }
+
+        public TreeNode buildTailTree2(int[] tailOrder, int[] inorder) {
+            Map<Integer, Integer> valIndexMap = new HashMap<>();
+            for (int i = 0; i < inorder.length; i++) {
+                valIndexMap.put(inorder[i], i);
+            }
+            return buildTail2(tailOrder, valIndexMap, 0, tailOrder.length - 1, 0);
+        }
+
+
+        /**
+         * 后续、中序构造二叉树
+         * 从前边开始计算长度(左子树)
+         */
+        public TreeNode buildTail(int[] postorder, Map<Integer, Integer> map, int postStart, int postEnd,
+                                  int inStart) {
+            if (postEnd < postStart) {
+                return null;
+            }
+            TreeNode root = new TreeNode(postorder[postEnd]);
+            int inRootIndex = map.get(root.val);
+            /**
+             * 左子树长度
+             */
+            int len = inRootIndex - inStart;
+            root.left = buildTail(postorder, map, postStart, postStart + len - 1, inStart);
+            root.right = buildTail(postorder, map, postStart + len, postEnd - 1, inRootIndex + 1);
+            return root;
+        }
+
+        /**
+         * 后续、中序构造二叉树
+         * 从右边开始计算长度(右子树)
+         */
+        public TreeNode buildTail2(int[] postorder, Map<Integer, Integer> map, int postStart, int postEnd,
+                                   int inEnd) {
+            if (postEnd < postStart) {
+                return null;
+            }
+            TreeNode root = new TreeNode(postorder[postEnd]);
+            int inRootIndex = map.get(root.val);
+            /**
+             * 右子树长度
+             */
+            int len = inEnd - inRootIndex;
+            root.left = buildTail2(postorder, map, postStart, postEnd - len - 1, inEnd - 1);
+            root.right = buildTail2(postorder, map, postEnd - len, postEnd - 1, len);
             return root;
         }
 
