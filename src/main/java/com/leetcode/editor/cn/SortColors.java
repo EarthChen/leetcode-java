b5bd6143c9514458f8f13a0e20f0c59257dfb684
earthchen <earthchen1996@gmail.com>
2021-04-04 17:25:30 +0800

    feat(leetcode): 75 颜色分类

---
+++ src/main/java/com/leetcode/editor/cn/SortColors.java
@@ -0,0 +1,97 @@
+package com.leetcode.editor.cn;
+
+//给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。 
+//
+// 此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。 
+//
+// 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,0,2,1,1,0]
+//输出：[0,0,1,1,2,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [2,0,1]
+//输出：[0,1,2]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [0]
+//输出：[0]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：nums = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// n == nums.length 
+// 1 <= n <= 300 
+// nums[i] 为 0、1 或 2 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 你可以不使用代码库中的排序函数来解决这道题吗？ 
+// 你能想出一个仅使用常数空间的一趟扫描算法吗？ 
+// 
+// Related Topics 排序 数组 双指针 
+// 👍 834 👎 0
+
+
+public class SortColors {
+    public static void main(String[] args) {
+        Solution solution = new SortColors().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void sortColors(int[] nums) {
+            int i = 0;
+            int part = 1;
+            int l = 0;
+            int r = nums.length - 1;
+            while (i <= r) {
+                int num = nums[i];
+                if (num == part) {
+                    i++;
+                } else if (num > part) {
+                    swap(nums, i, r);
+                    r--;
+                } else {
+                    swap(nums, i, l);
+                    i++;
+                    l++;
+                }
+            }
+        }
+
+        public void swap(int[] nums, int i, int j) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
