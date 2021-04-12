fcf5059adc1181675f5978973fdbddff3eb50794
earthchen <earthchen1996@gmail.com>
2021-04-12 21:35:50 +0800

    feat(leetcode): 128 最长连续序列

---
+++ src/main/java/com/leetcode/editor/cn/LongestConsecutiveSequence.java
@@ -0,0 +1,72 @@
+package com.leetcode.editor.cn;
+
+//给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。 
+//
+// 
+//
+// 进阶：你可以设计并实现时间复杂度为 O(n) 的解决方案吗？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [100,4,200,1,3,2]
+//输出：4
+//解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,3,7,2,5,8,4,6,0,1]
+//输出：9
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= nums.length <= 104 
+// -109 <= nums[i] <= 109 
+// 
+// Related Topics 并查集 数组 
+// 👍 740 👎 0
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        Solution solution = new LongestConsecutiveSequence().new Solution();
+        System.out.println(solution.longestConsecutive(new int[]{100,4,200,1,3,2}));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int longestConsecutive(int[] nums) {
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num);
+            }
+            int max = 0;
+            for (Integer integer : set) {
+                int curVal = integer;
+                if (set.contains(curVal - 1)) {
+                    continue;
+                }
+                int curMax = 1;
+                curVal++;
+                while (set.contains(curVal)) {
+                    curMax++;
+                    curVal++;
+                }
+                max = Math.max(curMax, max);
+            }
+            return max;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
