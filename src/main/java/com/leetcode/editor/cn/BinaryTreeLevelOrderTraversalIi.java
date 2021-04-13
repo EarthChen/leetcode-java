1a35fd284d0ab5fbc2f1be4cd956da2028f3202f
earthchen <earthchen1996@gmail.com>
2021-04-13 21:10:35 +0800

    feat(leetcode): 107 二叉树的层序遍历2(dfs 解法)

--- src/main/java/com/leetcode/editor/cn/BinaryTreeLevelOrderTraversalIi.java
+++ src/main/java/com/leetcode/editor/cn/BinaryTreeLevelOrderTraversalIi.java
@@ -1,35 +1,34 @@
 package com.leetcode.editor.cn;
 
-//给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历） 
+//给定一个二叉树，返回其节点值自底向上的层序遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
 //
-// 例如： 
-//给定二叉树 [3,9,20,null,null,15,7], 
+// 例如：
+//给定二叉树 [3,9,20,null,null,15,7],
 //
-// 
+//
 //    3
 //   / \
 //  9  20
 //    /  \
 //   15   7
-// 
 //
-// 返回其自底向上的层序遍历为： 
 //
-// 
+// 返回其自底向上的层序遍历为：
+//
+//
 //[
 //  [15,7],
 //  [9,20],
 //  [3]
 //]
-// 
-// Related Topics 树 广度优先搜索 
+//
+// Related Topics 树 广度优先搜索
 // 👍 426 👎 0
 
 
 import com.leetcode.editor.cn.tree.TreeNode;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class BinaryTreeLevelOrderTraversalIi {
@@ -44,32 +43,49 @@ public class BinaryTreeLevelOrderTraversalIi {
      * this.left = left; this.right = right; } }
      */
     class Solution {
+        List<List<Integer>> res = new ArrayList<>();
+
         public List<List<Integer>> levelOrderBottom(TreeNode root) {
-            List<List<Integer>> ret = new ArrayList<>();
-            LinkedList<TreeNode> queue = new LinkedList<>();
-            if (root != null) {
-                queue.push(root);
-            }
-            while (!queue.isEmpty()) {
-                int size = queue.size();
-                List<Integer> vals = new ArrayList<>();
-                for (int i = 0; i < size; i++) {
-                    TreeNode cur = queue.pollLast();
-                    vals.add(cur.val);
-                    TreeNode left = cur.left;
-                    TreeNode right = cur.right;
-                    if (left != null) {
-                        queue.push(left);
-                    }
-                    if (right != null) {
-                        queue.push(right);
-                    }
-                }
-                ret.add(0, vals);
-            }
-            return ret;
+            levelOrderBottom(root, 0);
+            return res;
         }
+
+        public void levelOrderBottom(TreeNode root, int level) {
+            if (root == null) {
+                return;
+            }
+            if (res.size() <= level) {
+                res.add(0, new ArrayList<>());
+            }
+            res.get(res.size() - level - 1).add(root.val);
+            levelOrderBottom(root.left, level + 1);
+            levelOrderBottom(root.right, level + 1);
+        }
+        // List<List<Integer>> ret = new ArrayList<>();
+        // LinkedList<TreeNode> queue = new LinkedList<>();
+        // if (root != null) {
+        //     queue.push(root);
+        // }
+        // while (!queue.isEmpty()) {
+        //     int size = queue.size();
+        //     List<Integer> vals = new ArrayList<>();
+        //     for (int i = 0; i < size; i++) {
+        //         TreeNode cur = queue.pollLast();
+        //         vals.add(cur.val);
+        //         TreeNode left = cur.left;
+        //         TreeNode right = cur.right;
+        //         if (left != null) {
+        //             queue.push(left);
+        //         }
+        //         if (right != null) {
+        //             queue.push(right);
+        //         }
+        //     }
+        //     ret.add(0, vals);
+        // }
+        // return ret;
     }
-//leetcode submit region end(Prohibit modification and deletion)
+    //leetcode submit region end(Prohibit modification and deletion)
 
 }
+
