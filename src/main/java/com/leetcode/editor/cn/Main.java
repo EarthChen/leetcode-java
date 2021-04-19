1676fb4cd3b8e3d41909b5db868dfbf9f9f117eb
earthchen <youwei.chen@duobei.com>
2021-04-19 13:31:46 +0800

    feat(leetcode): 8 字符串转换整数

---
+++ src/main/java/com/leetcode/editor/cn/Main.java
@@ -0,0 +1,17 @@
+package com.leetcode.editor.cn;
+
+/**
+ * @author earthchen
+ * @date 2021/4/13
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "b";
+        String c = "a" + "b";
+
+        String ab = "ab";
+        System.out.println(c == ab);
+    }
+}
