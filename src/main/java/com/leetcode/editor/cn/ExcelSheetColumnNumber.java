c3577ecf958d92b97b10606cdb5a41ef3f6d4d6b
earthchen <earthchen1996@gmail.com>
2021-04-23 21:19:00 +0800

    feat(leetcode): 171 Excel表列序号

---
+++ src/main/java/com/leetcode/editor/cn/ExcelSheetColumnNumber.java
@@ -0,0 +1,61 @@
+package com.leetcode.editor.cn;
+
+//给定一个Excel表格中的列名称，返回其相应的列序号。 
+//
+// 例如， 
+//
+//     A -> 1
+//    B -> 2
+//    C -> 3
+//    ...
+//    Z -> 26
+//    AA -> 27
+//    AB -> 28 
+//    ...
+// 
+//
+// 示例 1: 
+//
+// 输入: "A"
+//输出: 1
+// 
+//
+// 示例 2: 
+//
+// 输入: "AB"
+//输出: 28
+// 
+//
+// 示例 3: 
+//
+// 输入: "ZY"
+//输出: 701 
+//
+// 致谢： 
+//特别感谢 @ts 添加此问题并创建所有测试用例。 
+// Related Topics 数学 
+// 👍 219 👎 0
+
+
+public class ExcelSheetColumnNumber {
+    public static void main(String[] args) {
+        Solution solution = new ExcelSheetColumnNumber().new Solution();
+        System.out.println(solution.titleToNumber("AB"));
+        System.out.println(solution.titleToNumber("ZY"));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int titleToNumber(String columnTitle) {
+            int num = 0;
+            int x = 0;
+            for (int i = columnTitle.length() - 1; i >= 0; i--) {
+                num += Math.pow(26, x) * (columnTitle.charAt(i) - 'A' + 1);
+                x++;
+            }
+            return num;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
