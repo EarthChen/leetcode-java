f4e40c1e904398f41170d9563d65fc9ca25c4a49
earthchen <earthchen1996@gmail.com>
2021-04-26 20:38:07 +0800

    feat(leetcode): 230 二叉搜索树中的第 k 小的元素

---
+++ src/main/java/com/leetcode/editor/cn/KthSmallestElementInABst.java
@@ -0,0 +1,76 @@
+package com.leetcode.editor.cn;
+
+//给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第 k 个最小元素（从 1 开始计数）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,1,4,null,2], k = 1
+//输出：1
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [5,3,6,2,4,null,null,1], k = 3
+//输出：3
+// 
+//
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中的节点数为 n 。 
+// 1 <= k <= n <= 104 
+// 0 <= Node.val <= 104 
+// 
+//
+// 
+//
+// 进阶：如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化算法？ 
+// Related Topics 树 二分查找 
+// 👍 386 👎 0
+
+
+import com.leetcode.editor.cn.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KthSmallestElementInABst {
+    public static void main(String[] args) {
+        Solution solution = new KthSmallestElementInABst().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode right; TreeNode() {}
+     * TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left, TreeNode right) { this.val = val;
+     * this.left = left; this.right = right; } }
+     */
+    class Solution {
+
+
+        public int kthSmallest(TreeNode root, int k) {
+            List<Integer> ret = new ArrayList<>();
+            inOrder(root, ret);
+            return ret.get(k-1);
+        }
+
+        private void inOrder(TreeNode root, List<Integer> ret) {
+            if (root == null) {
+                return;
+            }
+            inOrder(root.left, ret);
+            ret.add(root.val);
+            inOrder(root.right, ret);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
