9f52d4818fa085a02d024e8f80c0db3f6eeeff1f
earthchen <earthchen1996@gmail.com>
2021-05-04 15:11:43 +0800

    feat(面试题): 08.02 迷路的机器人(dfs)

--- src/main/java/com/leetcode/editor/cn/RobotInAGridLcci.java
+++ src/main/java/com/leetcode/editor/cn/RobotInAGridLcci.java
@@ -43,61 +43,94 @@ public class RobotInAGridLcci {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<List<Integer>> pathWithObstacles(int[][] obstacleGrid) {
-            int length = obstacleGrid.length;
-            int width = obstacleGrid[0].length;
-            List<List<Integer>>[][] dp = new List[length][width];
-            for (int i = 0; i < length; i++) {
-                for (int j = 0; j < width; j++) {
-                    int cur = obstacleGrid[i][j];
-                    if (cur == 1) {
-                        dp[i][j] = new ArrayList<>();
-                        continue;
-                    }
-                    if (i == 0 && j == 0) {
-                        List<List<Integer>> ret = new ArrayList<>();
-                        List<Integer> preIndex = new ArrayList<>();
-                        preIndex.add(i);
-                        preIndex.add(j);
-                        ret.add(preIndex);
-                        dp[i][j] = ret;
-                    } else if (i == 0) {
-                        List<List<Integer>> prePath = dp[i][j - 1];
-                        if (prePath.isEmpty()) {
-                            dp[i][j] = new ArrayList<>();
-                        } else {
-                            extracted(dp, i, j, prePath);
-                        }
-                    } else if (j == 0) {
-                        List<List<Integer>> prePath = dp[i - 1][j];
-                        if (prePath.isEmpty()) {
-                            dp[i][j] = new ArrayList<>();
-                        } else {
-                            extracted(dp, i, j, prePath);
-                        }
-                    } else {
-                        List<List<Integer>> prePath1 = dp[i - 1][j];
-                        List<List<Integer>> prePath2 = dp[i][j - 1];
-                        if (prePath1.isEmpty() && prePath2.isEmpty()) {
-                            dp[i][j] = new ArrayList<>();
-                        } else if (!prePath1.isEmpty()) {
-                            extracted(dp, i, j, prePath1);
-                        } else {
-                            extracted(dp, i, j, prePath2);
-                        }
-                    }
-                }
-            }
-            return dp[length - 1][width - 1];
+            List<List<Integer>> ret = new ArrayList<>();
+            dfs(obstacleGrid, 0, 0, ret);
+            return ret;
+            // List<List<Integer>>[][] dp = new List[length][width];
+            // for (int i = 0; i < length; i++) {
+            //     for (int j = 0; j < width; j++) {
+            //         int cur = obstacleGrid[i][j];
+            //         if (cur == 1) {
+            //             dp[i][j] = new ArrayList<>();
+            //             continue;
+            //         }
+            //         if (i == 0 && j == 0) {
+            //             List<List<Integer>> ret = new ArrayList<>();
+            //             List<Integer> preIndex = new ArrayList<>();
+            //             preIndex.add(i);
+            //             preIndex.add(j);
+            //             ret.add(preIndex);
+            //             dp[i][j] = ret;
+            //         } else if (i == 0) {
+            //             List<List<Integer>> prePath = dp[i][j - 1];
+            //             if (prePath.isEmpty()) {
+            //                 dp[i][j] = new ArrayList<>();
+            //             } else {
+            //                 extracted(dp, i, j, prePath);
+            //             }
+            //         } else if (j == 0) {
+            //             List<List<Integer>> prePath = dp[i - 1][j];
+            //             if (prePath.isEmpty()) {
+            //                 dp[i][j] = new ArrayList<>();
+            //             } else {
+            //                 extracted(dp, i, j, prePath);
+            //             }
+            //         } else {
+            //             List<List<Integer>> prePath1 = dp[i - 1][j];
+            //             List<List<Integer>> prePath2 = dp[i][j - 1];
+            //             if (prePath1.isEmpty() && prePath2.isEmpty()) {
+            //                 dp[i][j] = new ArrayList<>();
+            //             } else if (!prePath1.isEmpty()) {
+            //                 extracted(dp, i, j, prePath1);
+            //             } else {
+            //                 extracted(dp, i, j, prePath2);
+            //             }
+            //         }
+            //     }
+            // }
+            // return dp[length - 1][width - 1];
         }
 
-        private void extracted(List<List<Integer>>[][] dp, int i, int j, List<List<Integer>> prePath1) {
-            List<Integer> preIndex = new ArrayList<>();
-            preIndex.add(i);
-            preIndex.add(j);
-            List<List<Integer>> ret = new ArrayList<>(prePath1);
-            ret.add(preIndex);
-            dp[i][j] = ret;
+        private boolean dfs(int[][] obstacleGrid, int i, int j, List<List<Integer>> ret) {
+            int length = obstacleGrid.length;
+            int width = obstacleGrid[0].length;
+            if (i < 0 || i >= length) {
+                return false;
+            }
+            if (j < 0 || j >= width) {
+                return false;
+            }
+            if (obstacleGrid[i][j] == 1) {
+                return false;
+            }
+            if (i == length - 1 && j == width - 1) {
+                List<Integer> cur = new ArrayList<>();
+                cur.add(i);
+                cur.add(j);
+                ret.add(cur);
+                return true;
+            }
+            if (i < length - 1 && dfs(obstacleGrid, i + 1, j, ret) ||
+                    j < width - 1 && dfs(obstacleGrid, i, j + 1, ret)) {
+                List<Integer> cur = new ArrayList<>();
+                cur.add(i);
+                cur.add(j);
+                ret.add(0, cur);
+                return true;
+            }
+            // 剪枝
+            obstacleGrid[i][j] = 1;
+            return false;
         }
+
+        // private void extracted(List<List<Integer>>[][] dp, int i, int j, List<List<Integer>> prePath1) {
+        //     List<Integer> preIndex = new ArrayList<>();
+        //     preIndex.add(i);
+        //     preIndex.add(j);
+        //     List<List<Integer>> ret = new ArrayList<>(prePath1);
+        //     ret.add(preIndex);
+        //     dp[i][j] = ret;
+        // }
     }
 
 
