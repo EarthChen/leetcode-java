6b0a9790e064065b7b75d0fe3975f01239c08ca9
earthchen <youwei.chen@duobei.com>
2021-05-10 13:32:29 +0800

    feat(剑指 offer): 28 对称的二叉树

---
+++ src/main/java/com/leetcode/editor/cn/DuiChengDeErChaShuLcof.java
@@ -0,0 +1,79 @@
+package com.leetcode.editor.cn;
+
+//请实现一个函数，用来判断一棵二叉树是不是对称的。如果一棵二叉树和它的镜像一样，那么它是对称的。 
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。 
+//
+// 1 
+// / \ 
+// 2 2 
+// / \ / \ 
+//3 4 4 3 
+//但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的: 
+//
+// 1 
+// / \ 
+// 2 2 
+// \ \ 
+// 3 3 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：root = [1,2,2,3,4,4,3]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 输入：root = [1,2,2,null,3,null,3]
+//输出：false 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 1000 
+//
+// 注意：本题与主站 101 题相同：https://leetcode-cn.com/problems/symmetric-tree/ 
+// Related Topics 树 
+// 👍 172 👎 0
+
+
+import com.leetcode.editor.cn.tree.TreeNode;
+
+public class DuiChengDeErChaShuLcof {
+    public static void main(String[] args) {
+        Solution solution = new DuiChengDeErChaShuLcof().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode right; TreeNode(int
+     * x) { val = x; } }
+     */
+    class Solution {
+        public boolean isSymmetric(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            return check(root.left, root.right);
+        }
+
+        private boolean check(TreeNode left, TreeNode right) {
+            if (left == null && right == null) {
+                return true;
+            }
+            if (left == null || right == null) {
+                return false;
+            }
+            if (left.val != right.val) {
+                return false;
+            }
+            return check(left.left, right.right) && check(left.right, right.left);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
