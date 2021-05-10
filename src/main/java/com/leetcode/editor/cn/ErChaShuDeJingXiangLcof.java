f77ed68aedeba03b6a014c3eb35c425ea935e2be
earthchen <youwei.chen@duobei.com>
2021-05-10 13:23:18 +0800

    feat(剑指 offer): 27 二叉树的镜像

---
+++ src/main/java/com/leetcode/editor/cn/ErChaShuDeJingXiangLcof.java
@@ -0,0 +1,65 @@
+package com.leetcode.editor.cn;
+
+//请完成一个函数，输入一个二叉树，该函数输出它的镜像。 
+//
+// 例如输入： 
+//
+// 4 
+// / \ 
+// 2 7 
+// / \ / \ 
+//1 3 6 9 
+//镜像输出： 
+//
+// 4 
+// / \ 
+// 7 2 
+// / \ / \ 
+//9 6 3 1 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：root = [4,2,7,1,3,6,9]
+//输出：[4,7,2,9,6,3,1]
+// 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 1000 
+//
+// 注意：本题与主站 226 题相同：https://leetcode-cn.com/problems/invert-binary-tree/ 
+// Related Topics 树 
+// 👍 130 👎 0
+
+
+import com.leetcode.editor.cn.tree.TreeNode;
+
+public class ErChaShuDeJingXiangLcof {
+    public static void main(String[] args) {
+        Solution solution = new ErChaShuDeJingXiangLcof().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode right; TreeNode(int
+     * x) { val = x; } }
+     */
+    class Solution {
+        public TreeNode mirrorTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+            TreeNode left = mirrorTree(root.right);
+            TreeNode right = mirrorTree(root.left);
+            root.left = left;
+            root.right = right;
+            return root;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
