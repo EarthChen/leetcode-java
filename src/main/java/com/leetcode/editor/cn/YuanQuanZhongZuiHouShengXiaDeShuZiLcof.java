dc30872a5d8416a10c64c6379de022944907b9b0
earthchen <youwei.chen@duobei.com>
2021-06-17 13:34:35 +0800

    feat(剑指offer): 62 圆圈中最后剩下的数字

--- src/main/java/com/leetcode/editor/cn/YuanQuanZhongZuiHouShengXiaDeShuZiLcof.java
+++ src/main/java/com/leetcode/editor/cn/YuanQuanZhongZuiHouShengXiaDeShuZiLcof.java
@@ -42,12 +42,21 @@ public class YuanQuanZhongZuiHouShengXiaDeShuZiLcof {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int lastRemaining(int n, int m) {
-            int ans = 0;
-            // 最后一轮剩下2个人，所以从2开始反推
-            for (int i = 2; i <= n; i++) {
-                ans = (ans + m) % i;
+            // int ans = 0;
+            // // 最后一轮剩下2个人，所以从2开始反推
+            // for (int i = 2; i <= n; i++) {
+            //     ans = (ans + m) % i;
+            // }
+            // return ans;
+            return f(n, m);
+        }
+
+        public int f(int n, int m) {
+            if (n == 1) {
+                return 0;
             }
-            return ans;
+            int x = f(n - 1, m);
+            return (m + x) % n;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
