174f7ce18e5b6488b9eaf0630218cd9d0bf65aa4
earthchen <youwei.chen@duobei.com>
2021-07-15 18:07:20 +0800

    feat(leetcode): 32 最长有效括号(栈解法)

--- src/main/java/com/leetcode/editor/cn/LongestValidParentheses.java
+++ src/main/java/com/leetcode/editor/cn/LongestValidParentheses.java
@@ -43,6 +43,9 @@ package com.leetcode.editor.cn;
 // 👍 1369 👎 0
 
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class LongestValidParentheses {
     public static void main(String[] args) {
         Solution solution = new LongestValidParentheses().new Solution();
@@ -57,22 +60,18 @@ public class LongestValidParentheses {
     class Solution {
         public int longestValidParentheses(String s) {
             int maxans = 0;
-            int length = s.length();
-            int[] dp = new int[length];
-            for (int i = 1; i < length; i++) {
-                if (s.charAt(i) == ')') {
-                    if (s.charAt(i - 1) == '(') {
-                        dp[i] = 2;
-                        if (i >= 2) {
-                            dp[i] += dp[i - 2];
-                        }
-                    } else if (i - dp[i - 1] > 0 && s.charAt(i - dp[i - 1] - 1) == '(') {
-                        dp[i] = dp[i - 1] + 2;
-                        if (i - dp[i - 1] >= 2) {
-                            dp[i] += dp[i - dp[i - 1] - 2];
-                        }
+            Deque<Integer> stack = new LinkedList<>();
+            stack.push(-1);
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(') {
+                    stack.push(i);
+                } else {
+                    stack.pop();
+                    if (stack.isEmpty()) {
+                        stack.push(i);
+                    } else {
+                        maxans = Math.max(maxans, i - stack.peek());
                     }
-                    maxans = Math.max(maxans, dp[i]);
                 }
             }
             return maxans;
