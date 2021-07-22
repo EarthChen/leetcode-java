b075a355192ef7bbef2916b5807143c822faf6ff
earthchen <youwei.chen@duobei.com>
2021-07-22 14:08:26 +0800

    feat(leetcode): 99 恢复二叉搜索树

---
+++ src/main/java/com/leetcode/editor/cn/RecoverBinarySearchTree.java
@@ -0,0 +1,101 @@
+package com.leetcode.editor.cn;
+
+//给你二叉搜索树的根节点 root ，该树中的两个节点被错误地交换。请在不改变其结构的情况下，恢复这棵树。 
+//
+// 进阶：使用 O(n) 空间复杂度的解法很容易实现。你能想出一个只使用常数空间的解决方案吗？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,3,null,null,2]
+//输出：[3,1,null,null,2]
+//解释：3 不能是 1 左孩子，因为 3 > 1 。交换 1 和 3 使二叉搜索树有效。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [3,1,4,null,null,2]
+//输出：[2,1,4,null,null,3]
+//解释：2 不能在 3 的右子树中，因为 2 < 3 。交换 2 和 3 使二叉搜索树有效。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树上节点的数目在范围 [2, 1000] 内 
+// -231 <= Node.val <= 231 - 1 
+// 
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 
+// 👍 509 👎 0
+
+
+import com.leetcode.editor.cn.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecoverBinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new RecoverBinarySearchTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode right; TreeNode() {}
+     * TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left, TreeNode right) { this.val = val;
+     * this.left = left; this.right = right; } }
+     */
+    class Solution {
+        public void recoverTree(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            inorder(root, ans);
+            int[] ret = findTwoSwapped(ans);
+            swap(root, ret[0], ret[1], 2);
+        }
+
+        private void inorder(TreeNode root, List<Integer> ans) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left, ans);
+            ans.add(root.val);
+            inorder(root.right, ans);
+        }
+
+        private void swap(TreeNode root, int x, int y, int count) {
+            if (root == null) {
+                return;
+            }
+            if (root.val == x || root.val == y) {
+                root.val = root.val == x ? y : x;
+                if (--count == 0) {
+                    return;
+                }
+            }
+            swap(root.right, x, y, count);
+            swap(root.left, x, y, count);
+        }
+
+        public int[] findTwoSwapped(List<Integer> nums) {
+            int n = nums.size();
+            int x = -1, y = -1;
+            for (int i = 0; i < n - 1; ++i) {
+                if (nums.get(i + 1) < nums.get(i)) {
+                    y = nums.get(i + 1);
+                    if (x == -1) {
+                        x = nums.get(i);
+                    } else {
+                        break;
+                    }
+                }
+            }
+            return new int[]{x, y};
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
