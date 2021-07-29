209cf442fd25150d1607a7712e64e2ec0c2a2774
earthchen <youwei.chen@duobei.com>
2021-07-29 13:14:40 +0800

    feat(leetcode): 515 在每个树行中找最大值

---
+++ src/main/java/com/leetcode/editor/cn/FindLargestValueInEachTreeRow.java
@@ -0,0 +1,113 @@
+package com.leetcode.editor.cn;
+
+//给定一棵二叉树的根节点 root ，请找出该二叉树中每一层的最大值。 
+//
+// 
+//
+// 示例1： 
+//
+// 
+//输入: root = [1,3,2,5,3,null,9]
+//输出: [1,3,9]
+//解释:
+//          1
+//         / \
+//        3   2
+//       / \   \  
+//      5   3   9 
+// 
+//
+// 示例2： 
+//
+// 
+//输入: root = [1,2,3]
+//输出: [1,3]
+//解释:
+//          1
+//         / \
+//        2   3
+// 
+//
+// 示例3： 
+//
+// 
+//输入: root = [1]
+//输出: [1]
+// 
+//
+// 示例4： 
+//
+// 
+//输入: root = [1,null,2]
+//输出: [1,2]
+//解释:      
+//           1 
+//            \
+//             2     
+// 
+//
+// 示例5： 
+//
+// 
+//输入: root = []
+//输出: []
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 二叉树的节点个数的范围是 [0,104] 
+// -231 <= Node.val <= 231 - 1 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 
+// 👍 137 👎 0
+
+
+import com.leetcode.editor.cn.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class FindLargestValueInEachTreeRow {
+    public static void main(String[] args) {
+        Solution solution = new FindLargestValueInEachTreeRow().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode right; TreeNode() {}
+     * TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left, TreeNode right) { this.val = val;
+     * this.left = left; this.right = right; } }
+     */
+    class Solution {
+        public List<Integer> largestValues(TreeNode root) {
+            List<Integer> ans = new ArrayList<>();
+            LinkedList<TreeNode> queue = new LinkedList<>();
+            if (root != null) {
+                queue.addLast(root);
+            }
+
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                int max = Integer.MIN_VALUE;
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.pollFirst();
+                    max = Math.max(node.val,max);
+                    if (node.left != null) {
+                        queue.addLast(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.addLast(node.right);
+                    }
+                }
+                ans.add(max);
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
