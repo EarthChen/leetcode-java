fdbdc58c067d07b11656c02ac6bc51320252da0d
earthchen <youwei.chen@duobei.com>
2021-08-14 19:30:58 +0800

    feat(剑指 offer 2):004 只出现一次的数字

---
+++ src/main/java/com/leetcode/editor/cn/WGki4K.java
@@ -0,0 +1,65 @@
+package com.leetcode.editor.cn;
+
+//给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,2,3,2]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,1,0,1,0,1,100]
+//输出：100
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 3 * 104 
+// -231 <= nums[i] <= 231 - 1 
+// nums 中，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 
+// 
+//
+// 
+//
+// 进阶：你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
+//
+// 
+//
+// 注意：本题与主站 137 题相同：https://leetcode-cn.com/problems/single-number-ii/ 
+// Related Topics 位运算 数组 
+// 👍 1 👎 0
+
+
+public class WGki4K {
+    public static void main(String[] args) {
+        Solution solution = new WGki4K().new Solution();
+        System.out.println(solution.singleNumber(new int[]{2, 2, 3, 2}));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int singleNumber(int[] nums) {
+            int ans = 0;
+            for (int i = 0; i < 32; i++) {
+                int sum = 0;
+                for (int num : nums) {
+                    sum += (num >> i & 1);
+                }
+                sum %= 3;
+                ans += (sum << i);
+            }
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
