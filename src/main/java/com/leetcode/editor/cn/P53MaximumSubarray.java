71cc9580f5df7d2bae4056c70b84d59b32e2a540
earthchen <earthchen1996@gmail.com>
2022-10-17 13:47:40 +0800

    feat(leetcode): 52 最大子数组和

---
+++ src/main/java/com/leetcode/editor/cn/P53MaximumSubarray.java
@@ -0,0 +1,79 @@
+//给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。 
+//
+// 子数组 是数组中的一个连续部分。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+//输出：6
+//解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1]
+//输出：1
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [5,4,-1,7,8]
+//输出：23
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁵ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// 
+//
+// 
+//
+// 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。 
+//
+// Related Topics 数组 分治 动态规划 👍 5374 👎 0
+
+
+package com.leetcode.editor.cn;
+
+//Java：最大子数组和
+public class P53MaximumSubarray {
+    public static void main(String[] args) {
+        Solution solution = new P53MaximumSubarray().new Solution();
+        int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int ret = solution.maxSubArray(nums);
+        System.out.println(ret);
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int maxSubArray(int[] nums) {
+            int len = nums.length;
+            // dp[i] 表示：以 nums[i] 结尾的连续子数组的最大和
+            int[] dp = new int[len];
+            dp[0] = nums[0];
+            int max = nums[0];
+
+            for (int i = 1; i < len; i++) {
+                if (dp[i - 1] > 0) {
+                    dp[i] = dp[i - 1] + nums[i];
+                } else {
+                    dp[i] = nums[i];
+                }
+                max = Math.max(max, dp[i]);
+            }
+            return max;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
