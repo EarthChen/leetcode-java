8a4b9929a2e7eb0fe674e39cdbb4daa4c5de1877
earthchen <earthchen1996@gmail.com>
2022-10-26 14:03:38 +0800

    feat(leetcode): 79 单词搜索

---
+++ src/main/java/com/leetcode/editor/cn/P79WordSearch.java
@@ -0,0 +1,110 @@
+//给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。 
+//
+// 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = 
+//"ABCCED"
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = 
+//"SEE"
+//输出：true
+// 
+//
+// 示例 3： 
+// 
+// 
+//输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = 
+//"ABCB"
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == board.length 
+// n = board[i].length 
+// 1 <= m, n <= 6 
+// 1 <= word.length <= 15 
+// board 和 word 仅由大小写英文字母组成 
+// 
+//
+// 
+//
+// 进阶：你可以使用搜索剪枝的技术来优化解决方案，使其在 board 更大的情况下可以更快解决问题？ 
+//
+// Related Topics 数组 回溯 矩阵 👍 1467 👎 0
+
+
+package com.leetcode.editor.cn;
+
+//Java：单词搜索
+public class P79WordSearch {
+    public static void main(String[] args) {
+        Solution solution = new P79WordSearch().new Solution();
+        char[][] board = new char[][]{
+                new char[]{'a', 'b', 'c', 'e'},
+                new char[]{'s', 'f', 'c', 's'},
+                new char[]{'a', 'd', 'e', 'e'},
+        };
+        String word = "abcb";
+        boolean exits = solution.exist(board, word);
+
+        System.out.println(exits);
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean exist(char[][] board, String word) {
+            boolean[][] visited = new boolean[board.length][board[0].length];
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (check(board, visited, i, j, word, 0)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public boolean check(char[][] board, boolean[][] visited, int i, int j, String s, int k) {
+            if (board[i][j] != s.charAt(k)) {
+                return false;
+            } else if (k == s.length() - 1) {
+                return true;
+            }
+            visited[i][j] = true;
+            int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+            boolean result = false;
+            for (int[] dir : directions) {
+                int newi = i + dir[0];
+                int newj = j + dir[1];
+                if (newi >= 0 && newi < board.length && newj >= 0 && newj < board[0].length) {
+                    if (!visited[newi][newj]) {
+                        boolean flag = check(board, visited, newi, newj, s, k + 1);
+                        if (flag) {
+                            result = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            visited[i][j] = false;
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
