6a144f56beb7bfb150075893a49f07b463a45daf
earthchen <earthchen1996@gmail.com>
2022-10-27 13:02:22 +0800

    feat(leetcode): 94 二叉树的中序遍历

---
+++ src/main/java/com/leetcode/editor/cn/P94BinaryTreeInorderTraversal.java
@@ -0,0 +1,80 @@
+//给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [1,null,2,3]
+//输出：[1,3,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶: 递归算法很简单，你可以通过迭代算法完成吗？ 
+//
+// Related Topics 栈 树 深度优先搜索 二叉树 👍 1605 👎 0
+
+
+package com.leetcode.editor.cn;
+
+import com.leetcode.editor.cn.struct.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Java：二叉树的中序遍历
+public class P94BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new P94BinaryTreeInorderTraversal().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left = left; this.right = right; } }
+     */
+    class Solution {
+
+        private List<Integer> ret = new ArrayList<>();
+
+        public List<Integer> inorderTraversal(TreeNode root) {
+            dfs(root);
+            return ret;
+        }
+
+        private void dfs(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            dfs(root.left);
+            ret.add(root.val);
+            dfs(root.right);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
