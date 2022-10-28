ed2b32319562efa3f357ee0300bc56df1923e5de
earthchen <earthchen1996@gmail.com>
2022-10-28 17:56:42 +0800

    feat(leetcode): 98 验证二叉搜索树

---
+++ src/main/java/com/leetcode/editor/cn/P98ValidateBinarySearchTree.java
@@ -0,0 +1,76 @@
+//给你一个二叉树的根节点 root ，判断其是否是一个有效的二叉搜索树。 
+//
+// 有效 二叉搜索树定义如下： 
+//
+// 
+// 节点的左子树只包含 小于 当前节点的数。 
+// 节点的右子树只包含 大于 当前节点的数。 
+// 所有左子树和右子树自身必须也是二叉搜索树。 
+// 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [2,1,3]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [5,1,4,null,null,3,6]
+//输出：false
+//解释：根节点的值是 5 ，但是右子节点的值是 4 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在[1, 10⁴] 内 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 1794 👎 0
+
+
+package com.leetcode.editor.cn;
+
+import com.leetcode.editor.cn.struct.TreeNode;
+
+//Java：验证二叉搜索树
+public class P98ValidateBinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new P98ValidateBinarySearchTree().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left = left; this.right = right; } }
+     */
+    class Solution {
+        long pre = Long.MIN_VALUE;
+        public boolean isValidBST(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            // 访问左子树
+            if (!isValidBST(root.left)) {
+                return false;
+            }
+            // 访问当前节点：如果当前节点小于等于中序遍历的前一个节点，说明不满足BST，返回 false；否则继续遍历。
+            if (root.val <= pre) {
+                return false;
+            }
+            pre = root.val;
+            // 访问右子树
+            return isValidBST(root.right);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
