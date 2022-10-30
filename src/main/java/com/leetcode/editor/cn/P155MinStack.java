f04f14bca02fc27c958250ccd5ebc5843ed83328
earthchen <earthchen1996@gmail.com>
2022-10-30 13:38:06 +0800

    feat(leetcode): 155 最小栈

---
+++ src/main/java/com/leetcode/editor/cn/P155MinStack.java
@@ -0,0 +1,107 @@
+//设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。 
+//
+// 实现 MinStack 类: 
+//
+// 
+// MinStack() 初始化堆栈对象。 
+// void push(int val) 将元素val推入堆栈。 
+// void pop() 删除堆栈顶部的元素。 
+// int top() 获取堆栈顶部的元素。 
+// int getMin() 获取堆栈中的最小元素。 
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入：
+//["MinStack","push","push","push","getMin","pop","top","getMin"]
+//[[],[-2],[0],[-3],[],[],[],[]]
+//
+//输出：
+//[null,null,null,null,-3,null,0,-2]
+//
+//解释：
+//MinStack minStack = new MinStack();
+//minStack.push(-2);
+//minStack.push(0);
+//minStack.push(-3);
+//minStack.getMin();   --> 返回 -3.
+//minStack.pop();
+//minStack.top();      --> 返回 0.
+//minStack.getMin();   --> 返回 -2.
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -2³¹ <= val <= 2³¹ - 1 
+// pop、top 和 getMin 操作总是在 非空栈 上调用 
+// push, pop, top, and getMin最多被调用 3 * 10⁴ 次 
+// 
+//
+// Related Topics 栈 设计 👍 1441 👎 0
+
+
+package com.leetcode.editor.cn;
+
+import java.util.LinkedList;
+
+//Java：最小栈
+public class P155MinStack {
+    public static void main(String[] args) {
+        // Solution solution = new P155MinStack().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class MinStack {
+
+
+        private LinkedList<Integer> stack = new LinkedList<>();
+
+        private LinkedList<Integer> minStack = new LinkedList<>();
+
+        public MinStack() {
+
+        }
+
+        public void push(int val) {
+            stack.push(val);
+            if (minStack.isEmpty()) {
+                minStack.push(val);
+            } else {
+                int curMin = minStack.peek();
+                minStack.push(Math.min(curMin, val));
+            }
+        }
+
+        public void pop() {
+            stack.pop();
+            minStack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
