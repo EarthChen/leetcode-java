895891ee85286e17c54e0429e99f9e0f8be290d7
earthchen <earthchen1996@gmail.com>
2022-11-01 20:20:02 +0800

    feat(leetcode): 337 打家劫舍3

---
+++ src/main/java/com/leetcode/editor/cn/P337HouseRobberIii.java
@@ -0,0 +1,80 @@
+//小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为
+// root 。 
+//
+// 除了
+// root 之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果 两个直接相连的
+//房子在同一天晚上被打劫 ，房屋将自动报警。 
+//
+// 给定二叉树的 root 。返回 在不触动警报的情况下 ，小偷能够盗取的最高金额 。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入: root = [3,2,3,null,3,null,1]
+//输出: 7 
+//解释: 小偷一晚能够盗取的最高金额 3 + 3 + 1 = 7 
+//
+// 示例 2: 
+//
+// 
+//
+// 
+//输入: root = [3,4,5,1,3,null,1]
+//输出: 9
+//解释: 小偷一晚能够盗取的最高金额 4 + 5 = 9
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 
+//
+// 
+// 树的节点数在 [1, 10⁴] 范围内 
+// 0 <= Node.val <= 10⁴ 
+// 
+//
+// Related Topics 树 深度优先搜索 动态规划 二叉树 👍 1467 👎 0
+
+
+package com.leetcode.editor.cn;
+
+import com.leetcode.editor.cn.struct.TreeNode;
+
+//Java：打家劫舍 III
+public class P337HouseRobberIii {
+    public static void main(String[] args) {
+        Solution solution = new P337HouseRobberIii().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left, TreeNode right) { this.val = val; this.left = left; this.right = right; } }
+     */
+    class Solution {
+        public int rob(TreeNode root) {
+            int[] rootStatus = dfs(root);
+            return Math.max(rootStatus[0], rootStatus[1]);
+        }
+
+        public int[] dfs(TreeNode node) {
+            if (node == null) {
+                return new int[]{0, 0};
+            }
+            int[] l = dfs(node.left);
+            int[] r = dfs(node.right);
+            int selected = node.val + l[1] + r[1];
+            int notSelected = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
+            return new int[]{selected, notSelected};
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
