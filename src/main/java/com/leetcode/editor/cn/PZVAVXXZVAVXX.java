d40b7eb36d041975aaca321aa6b8dc7d072cc128
earthchen <earthchen1996@gmail.com>
2022-11-06 15:51:25 +0800

    feat(offer2): 009 乘积小于 k 的子数组

---
+++ src/main/java/com/leetcode/editor/cn/PZVAVXXZVAVXX.java
@@ -0,0 +1,71 @@
+//给定一个正整数数组 nums和整数 k ，请找出该数组内乘积小于 k 的连续的子数组的个数。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: nums = [10,5,2,6], k = 100
+//输出: 8
+//解释: 8 个乘积小于 100 的子数组分别为: [10], [5], [2], [6], [10,5], [5,2], [2,6], [5,2,6]。
+//需要注意的是 [10,5,2] 并不是乘积小于100的子数组。
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: nums = [1,2,3], k = 0
+//输出: 0 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= nums.length <= 3 * 10⁴ 
+// 1 <= nums[i] <= 1000 
+// 0 <= k <= 10⁶ 
+// 
+//
+// 
+//
+// 
+// 注意：本题与主站 713 题相同：https://leetcode-cn.com/problems/subarray-product-less-than-
+//k/ 
+//
+// Related Topics 数组 滑动窗口 👍 111 👎 0
+
+
+package com.leetcode.editor.cn;
+
+//Java：乘积小于 K 的子数组
+public class PZVAVXXZVAVXX {
+    public static void main(String[] args) {
+        Solution solution = new PZVAVXXZVAVXX().new Solution();
+        int[] nums = new int[]{10, 5, 2, 6};
+        int ret = solution.numSubarrayProductLessThanK(nums, 100);
+
+        System.out.println(ret);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int numSubarrayProductLessThanK(int[] nums, int k) {
+            int n = nums.length;
+            int ret = 0;
+            int prod = 1;
+            int i = 0;
+            for (int j = 0; j < n; j++) {
+                prod *= nums[j];
+                while (i <= j && prod >= k) {
+                    prod /= nums[i];
+                    i++;
+                }
+                ret += j - i + 1;
+            }
+            return ret;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
