ebeb6db465fe33797f6592add6acde28300b27ad
earthchen <earthchen@apache.org>
2025-03-07 18:54:27 +0800

    feat(top100): 48 旋转图像

---
+++ src/main/java/com/leetcode/editor/cn/PRotateImage.java
@@ -0,0 +1,72 @@
+//给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。 
+//
+// 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+//输出：[[7,4,1],[8,5,2],[9,6,3]]
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+//输出：[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// n == matrix.length == matrix[i].length 
+// 1 <= n <= 20 
+// -1000 <= matrix[i][j] <= 1000 
+// 
+//
+// 
+//
+// Related Topics 数组 数学 矩阵 👍 2023 👎 0
+
+
+package com.leetcode.editor.cn;
+
+//Java：旋转图像
+public class PRotateImage {
+    public static void main(String[] args) {
+        Solution solution = new PRotateImage().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void rotate(int[][] matrix) {
+            int top = 0;
+            int bottom = matrix.length - 1;
+            int left = 0;
+            int right = matrix[0].length - 1;
+            while (top < bottom && left < right) {
+
+                for (int i = 0; i < right - left; i++) {
+                    int temp = matrix[top][left + i];
+                    matrix[top][left + i] = matrix[bottom - i][left];
+                    matrix[bottom - i][left] = matrix[bottom][right - i];
+                    matrix[bottom][right - i] = matrix[top + i][right];
+                    matrix[top + i][right] = temp;
+                }
+                top++;
+                bottom--;
+                left++;
+                right--;
+            }
+        }
+
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
