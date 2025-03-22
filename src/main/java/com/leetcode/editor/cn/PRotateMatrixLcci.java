9fb4dff1f248419ec2aa1bbda6fd70e2c1a606b2
earthchen <earthchen@apache.org>
2025-03-22 17:59:44 +0800

    feat(面试题): 01.07 旋转矩阵

---
+++ src/main/java/com/leetcode/editor/cn/PRotateMatrixLcci.java
@@ -0,0 +1,82 @@
+//给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。 
+//
+// 不占用额外内存空间能否做到？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//给定 matrix = 
+//[
+//  [1,2,3],
+//  [4,5,6],
+//  [7,8,9]
+//],
+//
+//原地旋转输入矩阵，使其变为:
+//[
+//  [7,4,1],
+//  [8,5,2],
+//  [9,6,3]
+//]
+// 
+//
+// 示例 2： 
+//
+// 
+//给定 matrix =
+//[
+//  [ 5, 1, 9,11],
+//  [ 2, 4, 8,10],
+//  [13, 3, 6, 7],
+//  [15,14,12,16]
+//], 
+//
+//原地旋转输入矩阵，使其变为:
+//[
+//  [15,13, 2, 5],
+//  [14, 3, 4, 1],
+//  [12, 6, 8, 9],
+//  [16, 7,10,11]
+//]
+// 
+//
+// 注意：本题与主站 48 题相同：https://leetcode-cn.com/problems/rotate-image/ 
+//
+// Related Topics 数组 数学 矩阵 👍 299 👎 0
+
+
+package com.leetcode.editor.cn;
+
+//Java：旋转矩阵
+public class PRotateMatrixLcci {
+    public static void main(String[] args) {
+        Solution solution = new PRotateMatrixLcci().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void rotate(int[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int top = 0;
+            int bottom = n - 1;
+            int left = 0;
+            int right = m - 1;
+            while (top < bottom && left < right) {
+                for (int i = 0; i < right - left; i++) {
+                    int tmp = matrix[top][left + i];
+                    matrix[top][left + i] = matrix[bottom - i][left];
+                    matrix[bottom - i][left] = matrix[bottom][right - i];
+                    matrix[bottom][right-i]=matrix[top+1][right];
+                    matrix[top+i][right]=tmp;
+                }
+            }
+
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
