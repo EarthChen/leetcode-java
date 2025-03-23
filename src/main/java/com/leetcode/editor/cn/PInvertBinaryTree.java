fc15aa2cf78a785a89c6b43e876e54e461e2c328
earthchen <earthchen@apache.org>
2025-03-23 15:33:03 +0800

    feat(hot100): 226 反转二叉树

---
+++ src/main/java/com/leetcode/editor/cn/PInvertBinaryTree.java
@@ -0,0 +1,89 @@
+//给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [4,2,7,1,3,6,9]
+//输出：[4,7,2,9,6,3,1]
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：root = [2,1,3]
+//输出：[2,3,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目范围在 [0, 100] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1939 👎 0
+
+
+package com.leetcode.editor.cn;
+
+//Java：翻转二叉树
+public class PInvertBinaryTree {
+    public static void main(String[] args) {
+        Solution solution = new PInvertBinaryTree().new Solution();
+        // TO TEST
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     */
+    class Solution {
+        public TreeNode invertTree(TreeNode root) {
+            if (root == null) {
+                return null;
+            }
+            TreeNode left = invertTree(root.left);
+            TreeNode right = invertTree(root.right);
+            root.left = right;
+            root.right = left;
+            return root;
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
