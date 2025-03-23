56fdb12966fbc6cd2e70ce951c832f0ff1e02138
earthchen <earthchen@apache.org>
2025-03-23 16:01:34 +0800

    feat(hot100): 322 零钱兑换

---
+++ src/main/java/com/leetcode/editor/cn/PCoinChange.java
@@ -0,0 +1,74 @@
+//给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。 
+//
+// 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。 
+//
+// 你可以认为每种硬币的数量是无限的。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：coins = [1, 2, 5], amount = 11
+//输出：3 
+//解释：11 = 5 + 5 + 1 
+//
+// 示例 2： 
+//
+// 
+//输入：coins = [2], amount = 3
+//输出：-1 
+//
+// 示例 3： 
+//
+// 
+//输入：coins = [1], amount = 0
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= coins.length <= 12 
+// 1 <= coins[i] <= 2³¹ - 1 
+// 0 <= amount <= 10⁴ 
+// 
+//
+// Related Topics 广度优先搜索 数组 动态规划 👍 3012 👎 0
+
+
+package com.leetcode.editor.cn;
+
+//Java：零钱兑换
+public class PCoinChange {
+    public static void main(String[] args) {
+        Solution solution = new PCoinChange().new Solution();
+        // TO TEST
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int coinChange(int[] coins, int amount) {
+            int[] dp = new int[amount + 1];
+            for (int i = 0; i <= amount; i++) {
+                dp[i] = Integer.MAX_VALUE - 1;
+            }
+            dp[0] = 0;
+            for (int i = 1; i <= amount; i++) {
+                for (int coin : coins) {
+                    if (coin <= i) {
+                        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                    }
+                }
+            }
+            if (dp[amount] > amount) {
+                return -1;
+            }
+            return dp[amount];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
