1ae8d3210cc388c3206c5b81b9f5bbbb667894f6
earthchen <earthchen@apache.org>
2025-03-25 13:42:20 +0800

    fix

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,3 @@
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * @author earthchen
@@ -7,41 +5,7 @@ import java.util.Set;
  **/
 public class Solution {
 
-    public int lengthOfLongestSubstring(String s) {
-        int length = s.length();
-        if (length == 0) {
-            return 0;
-        }
-        int maxLength = 1;
-        for (int i = 0; i < length; i++) {
-            Set<Character> charSet = new HashSet<>();
-            for (int j = i; j < length; j++) {
-                char currentChar = s.charAt(j);
-                if (!charSet.contains(currentChar)) {
-                    charSet.add(currentChar);
-                } else {
-                    int currentLength = charSet.size();
-                    if (currentLength > maxLength) {
-                        maxLength = currentLength;
-                    }
-                    break;
-                }
-                if (j == length - 1) {
-                    int currentLength = charSet.size();
-                    if (currentLength > maxLength) {
-                        maxLength = currentLength;
-                    }
-                }
-            }
-        }
-        return maxLength;
-    }
-
-
     public static void main(String[] args) {
-        System.out.println(Runtime.getRuntime().availableProcessors());
-        // Solution solution = new Solution();
-        // int length = solution.lengthOfLongestSubstring("au");
-        // System.out.println(length);
+
     }
 }
--- src/main/java/com/leetcode/editor/cn/PRotateMatrixLcci.java
+++ src/main/java/com/leetcode/editor/cn/PRotateMatrixLcci.java
@@ -59,20 +59,23 @@ public class PRotateMatrixLcci {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public void rotate(int[][] matrix) {
-            int m = matrix.length;
-            int n = matrix[0].length;
             int top = 0;
-            int bottom = n - 1;
+            int bottom = matrix.length - 1;
             int left = 0;
-            int right = m - 1;
+            int right = matrix[0].length - 1;
             while (top < bottom && left < right) {
+
                 for (int i = 0; i < right - left; i++) {
-                    int tmp = matrix[top][left + i];
+                    int temp = matrix[top][left + i];
                     matrix[top][left + i] = matrix[bottom - i][left];
                     matrix[bottom - i][left] = matrix[bottom][right - i];
-                    matrix[bottom][right-i]=matrix[top+1][right];
-                    matrix[top+i][right]=tmp;
+                    matrix[bottom][right - i] = matrix[top + i][right];
+                    matrix[top + i][right] = temp;
                 }
+                top++;
+                bottom--;
+                left++;
+                right--;
             }
 
         }
